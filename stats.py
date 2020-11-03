c89a118f440dd89bb1d17e004b332cc03b92967a
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-03 09:40:12 +0100

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,37 @@
+def moyenne(var, date1, date2) :
+    periode = select_lignes(date1, date2)
+    l_var = [ligne[var] for ligne in period]
+    moy = 0
+    for e in l_var :
+        moy = moy+e
+    moy = moy/len(l_var)
+    return moy
+
+def variance(var, date1, date2) :
+    periode = select_lignes(date1, date2)
+    l_var = [ligne[var] for ligne in period]
+    vari = 0
+    moy = moyenne(var, date1, date2)
+    for e in l_var :
+        vari = vari + pow(e-moy, 2)
+    vari = vari/len(l_var)
+    return vari
+
+def ecart_type(var, date1, date2) :
+    return sqrt(variance(var, date1, date2))
+
+def mediane(var, date1, date2) :
+    periode = select_lignes(date1, date2)
+    l_var = [ligne[var] for ligne in period]
+    i = len(l_var)//2
+    return l_var[i]
+
+def minimum(var, date1, date2) :
+    periode = select_lignes(date1, date2)
+    l_var = [ligne[var] for ligne in period]
+    return (min(l_var))
+
+def maximum(var, date1, date2) :
+    periode = select_lignes(date1, date2)
+    l_var = [ligne[var] for ligne in period]
+    return (min(l_var))

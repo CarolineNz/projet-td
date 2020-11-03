86b3c57a89e8de561be41ca5d1af774b52e394f9
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-03 10:14:26 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
-def moyenne(var, date1, date2) :
+def moyenne(nom_var, date1, date2) :
+    var = titres.index(nom_var)
     periode = select_lignes(date1, date2)
     l_var = [ligne[var] for ligne in periode]
     moy = 0
@@ -7,31 +8,36 @@ def moyenne(var, date1, date2) :
     moy = moy/len(l_var)
     return moy
 
-def variance(var, date1, date2) :
+def variance(nom_var, date1, date2) :
+    var = titres.index(nom_var)
     periode = select_lignes(date1, date2)
     l_var = [ligne[var] for ligne in periode]
     vari = 0
-    moy = moyenne(var, date1, date2)
+    moy = moyenne(nom_var, date1, date2)
     for e in l_var :
         vari = vari + pow(e-moy, 2)
     vari = vari/len(l_var)
     return vari
 
-def ecart_type(var, date1, date2) :
-    return sqrt(variance(var, date1, date2))
+def ecart_type(nom_var, date1, date2) :
+    return math.sqrt(variance(nom_var, date1, date2))
 
-def mediane(var, date1, date2) :
+def mediane(nom_var, date1, date2) :
+    var = titres.index(nom_var)
     periode = select_lignes(date1, date2)
+    print(var)
     l_var = [ligne[var] for ligne in periode]
     i = len(l_var)//2
     return l_var[i]
 
-def minimum(var, date1, date2) :
+def minimum(nom_var, date1, date2) :
+    var = titres.index(nom_var)
     periode = select_lignes(date1, date2)
     l_var = [ligne[var] for ligne in periode]
     return (min(l_var))
 
-def maximum(var, date1, date2) :
+def maximum(nom_var, date1, date2) :
+    var = titres.index(nom_var)
     periode = select_lignes(date1, date2)
     l_var = [ligne[var] for ligne in periode]
     return (min(l_var))

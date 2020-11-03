ac447e95ea6d735fbd141df6118190e860ec4b7c
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-03 10:14:39 +0100

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,7 +1,9 @@
-def correlation(var1, var2, date1, date2) :
+def correlation(nom_var1, nom_var2, date1, date2) :
     '''var1 et var2 les INDICES des variables (numéro de colonne)
-    Utilise tableau, select_lignes, moyenne et ecart_type
-    affiche les courbes des deux variables avec le coeff de corrélation, et renvoie le coeff de corrélation'''
+    utilise tableau, select_lignes, moyenne et ecart_type
+    affiche les courbes des variables et l'indice de corrélation, renvoie l'indice de corrélation'''
+    var1 = titres.index(nom_var1)
+    var2 = titres.index(nom_var2)
     periode = select_lignes(date1, date2)
 
     #CALCUL DU COEFFICIENT
@@ -17,15 +19,15 @@ def correlation(var1, var2, date1, date2) :
     moyp = moyp/len(periode)
 
     #calcul des deux autres espérences
-    moy1 = moyenne(var1, date1, date2)
-    moy2 = moyenne(var2, date1, date2)
+    moy1 = moyenne(nom_var1, date1, date2)
+    moy2 = moyenne(nom_var2, date1, date2)
 
     #calcul de la covariance
     cov = moyp - moy1*moy2
 
     #calcul des écart-types
-    sigma1 = ecart_type(var1, date1, date2)
-    sigma2 = ecart_type(var2, date1, date2)
+    sigma1 = ecart_type(nom_var1, date1, date2)
+    sigma2 = ecart_type(nom_var2, date1, date2)
 
     #calcul du coefficient de correlation
     r = cov/(sigma1*sigma2)
@@ -37,8 +39,8 @@ def correlation(var1, var2, date1, date2) :
     y2 = np.array(l_var2)
     x = np.array([i for i in range(len(l_var1))]) #car les mesures sont prises à intervalles réguliers
 
-    plt.plot(x, y1, label=titres[var1])
-    plt.plot(x, y2, label=titres[var2])
+    plt.plot(x, y1, label=nom_var1)
+    plt.plot(x, y2, label=nom_var2)
     plt.legend()
     plt.title("Coefficient de corrélation : "+str(r))
     plt.show()

01a08ae0c588bbc82aafa9e03408c1c759e6b318
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-03 10:14:37 +0100

    Update display.py

--- display.py
+++ display.py
@@ -1,9 +1,7 @@
-
-def display(var, date1, date2) :
-    '''var INDICE de la variable (numéro de colonne)
-    Utilise tableau, select_lignes
+def display(nom_var, date1, date2) :
+    '''Utilise tableau, select_lignes
     affiche la courbe d'une des variables'''
-    
+    var = titres.index(nom_var)
     #RECUPERATION DE LA VARIABLE UTILE
     periode = select_lignes(date1, date2)
     l_var = [ligne[var] for ligne in period]
@@ -12,9 +10,9 @@ def display(var, date1, date2) :
     #AFFICHAGE DE LA COURBE
     plt.close()
     y = np.array(l_var)
-    x = np.array([i for i in range(len(l_var))]) 
-    
+    x = np.array([i for i in range(len(l_var))])
+
     plt.plot(x, y, label=titres[var])
     plt.legend()
- 
+
     plt.show()

e52edd98362c78a0d55fc1281ec6965e77dd8835
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-17 11:04:01 +0100

    Update select_lignes.py

--- select_lignes.py
+++ select_lignes.py
@@ -2,8 +2,11 @@
 
 def select_lignes(date1, date2) :
     '''Crée un nouveau tableau "periode" avec les lignes du tableau de base comprises entre les deux dates incluses '''
-    periode = []
-    for ligne in tableau :
-        if ligne[-1]>=date1 and ligne[-1]<=date2 :
-            periode.append(ligne)
-    return periode
+    if date1=="2019-08-11 11:30:50+02:00" and date2=="2019-08-25 17:47:08+02:00" :
+        return tableau
+    else :
+        periode = []
+        for ligne in tableau :
+            if ligne[-1]>=date1 and ligne[-1]<=date2 :
+                periode.append(ligne)
+        return periode

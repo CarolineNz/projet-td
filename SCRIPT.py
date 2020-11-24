c5b7ed06ec5070295057169464dfbde921a78904
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-24 16:48:08 +0100

    Update SCRIPT.py

--- SCRIPT.py
+++ SCRIPT.py
@@ -244,21 +244,19 @@ def humidex(date1="2019-08-11 11:30:50+02:00", date2="2019-08-25 17:47:08+02:00"
 ##COMPARAISON DES CAPTEURS
 
 ##séparation des capteurs
+
+def select_capteur(id) :
+    capteur = []
+    for ligne in tableau :
+        if ligne[1] == id :
+            capteur.append(ligne)
+    return capteur
+
 def capteurs() :
-
-    def select_capteur(id) :
-        capteur = []
-        for ligne in tableau :
-            if ligne[1] == id :
-                capteur.append(ligne)
-        return capteur
-
     l_capteurs = []
     for id in range(1, 7) :
         l_capteurs.append(select_capteur(id))
     return l_capteurs
-
-#AFFICHAGE DES COURBES
 
 #tri du tableau par dates croissantes
 def tri() :
@@ -314,6 +312,55 @@ def display_all(nom_var) :
 
     return
 
+##Position des capteurs
+def analyse_positions() :
+    l_capteurs = capteurs()
+    #calcul des moyennes de chaque valeur :
+    moy_co2 = moyenne("co2")
+    moy_lum = moyenne("lum")
+    ecarts_co2 = []
+    ecarts_lum = []
+    for capt in l_capteurs :
+        #calcul des moyennes pour le capteur :
+        moy_capt_co2=0
+        moy_capt_lum=0
+        i_co2 = titres.index("co2")
+        i_lum = titres.index("lum")
+        for ligne in capt :
+            moy_capt_co2 = moy_capt_co2+ligne[i_co2]
+            moy_capt_lum = moy_capt_lum+ligne[i_lum]
+        moy_capt_co2 = moy_capt_co2/len(capt)
+        moy_capt_lum = moy_capt_lum/len(capt)
+        #calcul des écarts aux moyennes pour ce capteur :
+        ecarts_co2.append(moy_co2-moy_capt_co2)
+        ecarts_lum.append(moy_lum-moy_capt_lum)
+
+    #affichage :
+    plt.clf()
+    for i in range(6) :
+        #un point de couleur par capteur
+        plt.scatter(ecarts_co2[i], ecarts_lum[i], label="capteur"+str(i+1))
+    plt.legend()
+    axes = plt.gca()
+
+    #affichages d'axes x=0 et y=0
+    axes = plt.gca()
+    xmin, xmax = axes.get_xlim()
+    plt.plot([xmin, xmax],[0, 0], color="k")
+    ymin, ymax = axes.get_ylim()
+    plt.plot([0, 0], [ymin, ymax], color="k")
+
+    #affichage du texte
+    plt.text(xmin,2, "Loin de la rue")
+    plt.text(xmax-10,2, "Près de la rue")
+    plt.text(0,ymin, "Nord")
+    plt.text(0,ymax, "Sud")
+
+    plt.show()
+
+
+    return
+
 ## INTERFACE
 
 (titres, tableau) = ouvrir_fichier()
@@ -338,5 +385,9 @@ elif action == "corrélation" :
         correlation(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
     else :
         correlation(sys.argv[2], sys.argv[3])
+elif action == "displayAll" :
+    display_all(sys.argv[2])
+elif action == "analyse_position" :
+    analyse_positions()
 else :
     print("Entrée invalide.")

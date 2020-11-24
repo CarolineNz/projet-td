f1ad313d8b2d605a71581f70234eb202687a49c5
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-11-24 17:01:20 +0100

    Update SCRIPT.py

--- SCRIPT.py
+++ SCRIPT.py
@@ -355,6 +355,7 @@ def analyse_positions() :
     plt.text(xmax-10,2, "Près de la rue")
     plt.text(0,ymin, "Nord")
     plt.text(0,ymax, "Sud")
+    plt.title("Analyse en fonction du co2 et de la luminosité")
 
     plt.show()
 

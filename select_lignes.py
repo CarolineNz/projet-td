3373f4ecf2606b974501a909f3a207ef8f7520b9
sophiedlr <72785640+sophiedlr@users.noreply.github.com>
2020-11-28 21:04:45 +0100

    Add files via upload

--- select_lignes.py
+++ select_lignes.py
@@ -1,12 +1,18 @@
-#tableau = var globale
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 28 20:59:55 2020
 
-def select_lignes(date1, date2) :
+@author: Sophie
+"""
+
+##sélection d'une durée entre deux dates pour un capteur donné
+def select_lignes(id, date1, date2) :
     '''Crée un nouveau tableau "periode" avec les lignes du tableau de base comprises entre les deux dates incluses '''
     if date1=="2019-08-11 11:30:50+02:00" and date2=="2019-08-25 17:47:08+02:00" :
-        return tableau
+        return select_capteur(id)
     else :
         periode = []
-        for ligne in tableau :
+        for ligne in select_capteur(id) :
             if ligne[-1]>=date1 and ligne[-1]<=date2 :
                 periode.append(ligne)
         return periode

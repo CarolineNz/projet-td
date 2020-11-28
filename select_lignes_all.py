78acc990b6de1d77651528b62bf0dba8df4ac6da
sophiedlr <72785640+sophiedlr@users.noreply.github.com>
2020-11-28 21:16:01 +0100

    Add files via upload

--- select_lignes_all.py
+++ select_lignes_all.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 28 20:59:55 2020
-
-@author: Sophie
-"""
-
-##sélection d'une durée entre deux dates pour tous les capteurs
 def select_lignes_all(date1, date2) :
     '''Crée un nouveau tableau "periode" avec les lignes du tableau de base comprises entre les deux dates incluses '''
     if date1=="2019-08-11 11:30:50+02:00" and date2=="2019-08-25 17:47:08+02:00" :

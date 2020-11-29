1a1393fc37f3a03733638bf43470f128743abc98
sophiedlr <72785640+sophiedlr@users.noreply.github.com>
2020-11-29 10:45:19 +0100

    Add files via upload

--- analyse_moy.py
+++ analyse_moy.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 24 17:25:36 2020
-
-@author: Sophie
-"""
-
-def analyse_moy(nom_var):
+def analyse_moy(nom_var, date1="2019-08-11 11:30:50+02:00", date2="2019-08-25 17:47:08+02:00"):
     sup=[]
     inf=[]
     for i in range(1,6):
-        if moyenne_bis(nom_var,i)>=moyenne(nom_var):
+        if moyenne(nom_var,i,date1,date2)>=moyenne_all(nom_var,date1,date2):
             sup.append(i)
-        else : 
+        else :
             inf.append(i)
-    return sup,inf 
+    print("Supérieur à la moyenne :",sup)
+    print("Inférieur à la moyenne :",inf)
+    return
+
+def analyse_moy_all():
+    for i in titres[2:-1]:
+        print(i)
+        analyse_moy(i)
+        print("\n")
+
+    return

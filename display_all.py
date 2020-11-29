64bb485bd8ec7a77280d0e67cba6dec96dbce49c
sophiedlr <72785640+sophiedlr@users.noreply.github.com>
2020-11-29 11:36:46 +0100

    Add files via upload

--- display_all.py
+++ display_all.py
@@ -1,39 +1,19 @@
-def display_all(nom_var) :
+def display_all(nom_var, date1="2019-08-11 11:30:50+02:00", date2="2019-08-25 17:47:08+02:00") :
+    '''Utilise tableau, select_lignes
+    affiche la courbe d'une des variables'''
     var = titres.index(nom_var)
-    tab_trie = tri()
-    capts = [[] for i in range(6)] #une liste par capteur
-    xcapts = [[] for i in range(6)] #une liste d'abcisses par capteur
-    kdate = 0 #servira pour les abcisses parce que sinon ça sera pas classé par ordre croissant
-    dates = [] #servira pour la légende des x
-    jours = [] #légende finale avec juste les jours
+    
+    #RECUPERATION DE LA VARIABLE UTILE
+    periode = select_lignes_all(date1, date2)
+    l_var = [ligne[var] for ligne in periode]
 
-    for ligne in tab_trie :
-        dates.append(ligne[-1])
-        kdate = kdate+1
-        id = ligne[1]
-        for i in range(6) : #ajout de la valeur dans la liste du capteur correspondant
-            if i == id :
-                capts[i].append(ligne[var])
-                xcapts[i].append(kdate)
-
-
-    #remplissage de la liste "jours" pour la légende
-    jours.append(dates[0][:10]) #"AAAA-MM-JJ"
-    mem = jours[0]
-    for i in range(1,len(dates)) :
-        if not re.match(mem,dates[i]) :
-            jours.append(dates[i][:10])
-            mem = dates[i][:10]
-        else :
-            jours.append("")
-
-    #affichage sous forme de graphique
+    #AFFICHAGE DE LA COURBE
     plt.close()
-    for i in range(6) :
-        plt.plot(np.array(xcapts[i]), np.array(capts[i]),label="capteur "+str(i+1))
-    plt.xticks(range(len(jours)), jours, rotation=45)
-    plt.title(nom_var)
-    plt.legend()
-    plt.show()
+    y = np.array(l_var)
+    x = np.array([i for i in range(len(l_var))])
 
-    return
+    plt.plot(x, y, label=titres[var])
+    plt.ylabel(nom_var)
+    plt.legend()
+
+    plt.show()
